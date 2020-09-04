0d5e57b07e06ca650843c028bbf299567d800b94
Rudolf_Barbu <Rudolf_Barbu@outlook.com>
2020-09-05 00:31:58 +0300

    Improving project structure

---
+++ client/src/main/java/org/bsoftware/beagle/client/Beagle.java
@@ -0,0 +1,24 @@
+package org.bsoftware.beagle.client;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Beagle is a JavaFX application class
+ *
+ * @author Rudolf Barbu
+ * @version 1.0.0
+ */
+public class Beagle extends Application
+{
+    /**
+     * Main method of JavaFX application
+     *
+     * @param primaryStage delegated stage
+     */
+    @Override
+    public void start(Stage primaryStage)
+    {
+        primaryStage.show();
+    }
+}
---
+++ server/src/main/java/org/bsoftware/beagle/server/Beagle.java
@@ -0,0 +1,25 @@
+package org.bsoftware.beagle.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * Beagle is a Spring Boot application class
+ *
+ * @author Rudolf Barbu
+ * @version 1.0.0
+ */
+@SpringBootApplication
+public class Beagle extends SpringBootServletInitializer
+{
+    /**
+     * Entry point of Ward application
+     *
+     * @param args Spring Boot application arguments
+     */
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Beagle.class, args);
+    }
+}
